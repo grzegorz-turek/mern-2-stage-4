e1cdffcdeaf0415a62f0e22953fcf70b71bab5a6
Grzegorz Turek <grzegorz.s.turek@gmail.com>
2019-11-07 20:46:12 +0100

    Final

---
+++ client/src/components/pages/AddPost/AddPostPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PostForm from '../../features/PostForm/PostFormContainer';
+
+const AddPostPage = () => (
+    <div>
+        <h1>Add Post Page</h1>
+        <PostForm />
+    </div>
+);
+
+export default AddPostPage;
